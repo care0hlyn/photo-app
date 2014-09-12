e67690405f00695d1f5fe1def5aac260cdb68f20
Dustin Lewis & Justin Pacubas & Carolyn Huynh <student@epicodus.com>
2014-09-12 12:10:03 -0700

    Complete update operation for user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,12 +18,38 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       redirect_to root_url, notice: "Thank you for signing up!"
     else
       render "index"
     end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_url, notice: "Account deleted."
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to :back, notice: "Account updated."
+    else
+      binding.pry
+      redirect_to :back, alert: "Update failed!"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :password, :presence => { :on => :create }
-  validates :password_confirmation, :presence => true
+  validates :password_confirmation, :presence => { :on => :create }
   validates_uniqueness_of :email
 
   has_many :photos
